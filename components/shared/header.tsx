723a09636847d5bc5a98e268c403aa534e49ebf8
waitletmecook <zzula5152@gmail.com>
2025-02-13 12:25:13 +0300

    third commit

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Container } from './container';
 import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
+import { ShoppingCart, UserRound } from 'lucide-react';
+import { Carter_One } from 'next/font/google';
 
 
 interface Props {
@@ -21,8 +23,13 @@ interface Props {
                     </div>
                 </div>
                 {/* Правый блок */}
-                <div className=''>
-                    <Button variant='outline' className=''>Войти</Button>
+                <div className='flex gap-4'>
+                    <div className=''>
+                        <Button variant='outline' className='gap-1 flex items-center'><UserRound size={13}/>Войти</Button>
+                    </div>
+                    <div>
+                        <Button variant='outline' className=''><ShoppingCart size={13}/></Button>
+                    </div>
                 </div>
             </Container>
         </header>
