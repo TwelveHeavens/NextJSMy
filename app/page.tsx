79f6e75e54c10725271e1f3973c4c195df80601d
waitletmecook <zzula5152@gmail.com>
2025-02-20 12:30:43 +0300

    fourth commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import { Categories, Container, Title } from "@/components/shared";
+
+
+
 export default function Home() {
   return (
     <>
+    <Container>
+      <Title text='Все пиццы' size='lg' className="font-bold"/>
+    </Container>
+    <Container>
+      <Categories/>
+    </Container>
     
     </>
   );
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,1 +1,4 @@
-export {Container} from './container'
+export { Container } from './container'
+export { Header } from './header'
+export { Title } from './title'
+export { Categories } from './categories'
