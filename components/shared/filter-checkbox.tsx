7ad351a105e57ee465cf8783ffdb4c9843b68c27
waitletmecook <zzula5152@gmail.com>
2025-02-28 15:52:56 +0300

    seventh commit

--- components/shared/filter-checkbox.tsx
+++ components/shared/filter-checkbox.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Checkbox } from '@radix-ui/react-checkbox';
+import { Checkbox } from '../ui/checkbox';
 export interface FilterCheckboxProps {
     text: string;
     value: string;
     endAdornment?: React.ReactNode;
     onCheckedChange?: (checked: boolean) => void;
     checked?: boolean;
+}
+
+export const FilterCheckbox: React.FC<FilterCheckboxProps> = ({
+    text,
+    value,
+    endAdornment,
+    onCheckedChange,
+    checked
+}) => {
+    return (
+        <div className="flex items-center space-x-2">
+            <Checkbox
+            onCheckedChange={onCheckedChange}/>
+        </div>
+    )
 }
