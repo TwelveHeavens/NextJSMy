42499e034ef0461b1d61054425362f96e4dc4fbb
waitletmecook <zzula5152@gmail.com>
2025-03-17 16:35:28 +0300

    fourteenth commit

---
+++ components/shared/product-card.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+    className?:string;
+}
+
+export const ProductCard: React.FC<Props> = ({id, name, price, imageUrl, className}) => {
+    return (
+        
+    )
+}
