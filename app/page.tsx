7a216b9b0e9f7ccd6d38dbf31cf9439c0e77a1f4
waitletmecook <zzula5152@gmail.com>
2025-03-20 12:30:28 +0300

    fifteenth commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Categories, Container, SortPopup, Title } from "@/components/shared";
 import { Filters } from "@/components/shared/filters";
+import { ProductCard } from "@/components/shared/product-card";
 import { TopBar } from "@/components/shared/top-bar";
 
 
@@ -14,6 +15,15 @@ export default function Home() {
     <Container>
       <Filters/>
     </Container>
+    {/* список товаров */}
+    <div className="flex-1">
+      <div className="flex flex-col gap-16">
+        <ProductCard id={0}
+        name="Чизбургер-пицца"
+        price={550}
+        imageUrl=""/>
+      </div>
+    </div>
     </>
   );
 }
